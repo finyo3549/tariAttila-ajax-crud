d12f8ec29e38cb1b0114a96d327eba1eb4dd21a6
Tari Attila <finyo3549@gmail.com>
2023-11-15 08:40:15 +0100

    bootstrap import a main.js-ben

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle'
 const api_url = "https://retoolapi.dev/YZM9ys/data";
 document.addEventListener("DOMContentLoaded", (init) );
 function init () {
   const data = document.getElementById("data");
   loadTable(data);
+  uploadData();
 }
 
 function loadTable(data) {
@@ -28,4 +31,32 @@ function loadTable(data) {
     data.appendChild(tableRow);
     });
   })
+}
+
+function uploadData() {
+  const gomb = document.getElementById("gomb");
+  
+
+  gomb.addEventListener("click" , () => {
+    const name = document.getElementById("name").value;
+    const entry_date = document.getElementById("entry_date").value;
+    const job_title = document.getElementById("job_title").value;
+    const imei = document.getElementById("imei").value;
+    const postData = {
+      name: name,
+      entry_date: entry_date,
+      job_title: job_title,
+      imei: imei,
+    };
+
+    fetch(api_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+       
+
+  });
 }
